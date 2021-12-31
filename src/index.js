b067657f666ff04a41c054b56be045685d0d4e50
Ricky Chen <ryc8889@gmail.com>
2021-12-31 14:02:53 -0500

    Update client url & commit package-lock.json

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://odyssey-part5-ryc8889.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
